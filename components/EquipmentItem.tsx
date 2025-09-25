9083ad462b0c877dce2aec85d343153779797753
ljiskrito <2311718442@qq.com>
2025-09-25 17:01:54 +0800

    获取地理位置

--- components/EquipmentItem.tsx
+++ components/EquipmentItem.tsx
@@ -1,41 +1,24 @@
 import { EquipmentMenuProps } from "@/constants/types";
+import { router } from 'expo-router';
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Icon } from "react-native-paper";
 
 export default function EquipmentItem({ equipmentMenu }: EquipmentMenuProps) {
+
   return (
-    <TouchableOpacity
-      style={styles.profileList}
-      onPress={() => console.log(`Clicked on ${equipmentMenu.title}`)}
-    >
-      <View style={styles.leftContent}>
-        <Icon source={equipmentMenu.icon} size={24} />
-        <Text style={styles.profileListContent}>{equipmentMenu.title}</Text>
-      </View>
-      <Icon source="chevron-right" size={24} />
+    <TouchableOpacity 
+    style={styles.profileList} 
+    onPress={() => {router.push(equipmentMenu.link)}}>
+        <View style={styles.leftContent}>
+          <Icon source={equipmentMenu.icon} size={24} />
+          <Text style={styles.profileListContent}>{equipmentMenu.title}</Text>
+        </View>
+        <Icon source="chevron-right" size={24} />
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#eee",
-  },
-  bar: {
-    elevation: 4,
-
-    // iOS 阴影
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-
-    // 确保阴影可见（iOS需要设置背景色）
-    zIndex: 1,
-  },
-  barIcon: {
-    backgroundColor: "white",
-  },
   profileList: {
     backgroundColor: "#FFFFFF",
     flexDirection: "row", // 横向排列
--- constants/types.ts
+++ constants/types.ts
@@ -1,9 +1,11 @@
+import { RelativePathString } from "expo-router";
 
 // define a type for the equipment menu
 export type EquipmentMenu = {
     id: string;
     title: string;
     icon: string;
+    link: RelativePathString ;
 };
 
 // define props for the EquipmentItem component
